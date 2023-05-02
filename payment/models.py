9a94770fc66e16d2dc617bc094f5a5c84986e360
merehanyehia <merehan0yehia@gmail.com>
2023-05-02 22:00:54 +0300

    changes made in Payment model done

--- payment/models.py
+++ payment/models.py
@@ -4,7 +4,7 @@ from orders.models import Order
 # Create your models here.
 class Payment(models.Model):
     payment_id = models.BigAutoField(primary_key=True)
-    order = models.ForeignKey(Order, on_delete=models.CASCADE,related_name='orders_payment')
+    oid = models.ForeignKey(Order, on_delete=models.CASCADE,related_name='orders_payment')
     payment_date = models.DateTimeField(auto_now_add=True)
     payment_method_choices=[
         ("online","online"),
@@ -13,7 +13,7 @@ class Payment(models.Model):
     payment_method = models.CharField(max_length=50,choices= payment_method_choices ,default='online')
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     isPaid = models.BooleanField
-    shipping_Price = models.DecimalField(default=50)
+    shipping_Price = models.IntegerField(default=50)
 
     def get_total_amount(self):
         return (self.amount - self.shipping_Price)
