c9656de608712a5f1a54888e3372625c0619eb68
merehanyehia <merehan0yehia@gmail.com>
2023-05-07 23:09:52 +0300

    changes made in orders view and serializers

--- orders/serializers.py
+++ orders/serializers.py
@@ -1,12 +1,36 @@
+from orders.models import OrderItem
+from orders.models import Order
 from rest_framework import serializers
 
 
 class OrderSerializer(serializers.Serializer):
+    order_items = Order_ItemSerializer(many=True)
+
+    class Meta:
+        model = Order
+        fields = ['order_id', 'uid', 'createdAt', 'cancellation_deadline',   'shipping_address', 'cancellation_fees', 'phone_number', 'status', 'order_items']
     order_id = serializers.IntegerField(read_only=True)
     uid = serializers.IntegerField(read_only=True)
     createdAt = serializers.DateTimeField()
     cancellation_deadline = serializers.DateTimeField()
     shipping_address = serializers.CharField()
-    billing_address = serializers.CharField()
     phone_number = serializers.CharField()
     status = serializers.CharField()
+
+
+
+class Order_ItemSerializer(serializers.Serializer):
+    cart_id = serializers.IntegerField(read_only=True)
+    quantity = serializers.IntegerField()
+    price = serializers.DecimalField(max_digits=10, decimal_places=2)
+    image= serializers.ImageField(upload_to='order_item')
+    class Meta:
+        model = OrderItem
+        fields = ['cart_id', 'quantity', 'price', 'image']
+
+    def create(self, validated_data):
+        order_items_data = validated_data.pop('order_items')
+        order = Order.objects.create(**validated_data)
+        for order_item_data in order_items_data:
+            OrderItem.objects.create(order=order, **order_item_data)
+        return order
