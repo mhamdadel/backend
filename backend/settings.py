3f550f8c9adddeaaaed18c85549acfffa7d0fd3d
Muhammed Adel <muhammed.adel.elshall@gmail.com>
2023-05-07 16:02:46 +0300

    add postgreql

--- backend/settings.py
+++ backend/settings.py
@@ -119,10 +119,14 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+  'default': {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': 'neondb',
+    'USER': 'muhammed.adel.elshall',
+    'PASSWORD': 'SmMQPk14sTjv',
+    'HOST': 'ep-square-shape-508845.us-east-2.aws.neon.tech',
+    'PORT': '5432',
+  }
 }
 
 
