dadbd28d7abb2dee9b41433ec13c46e9df8344e1
MennaRaafat <menna.raafat.hamed@commerce.helwan.edu.eg>
2023-05-10 02:28:31 +0300

    Refactoring view function and testing it

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -2,6 +2,7 @@ from django.urls import path,include
 from . import views
 
 urlpatterns=[
-  path('',views.wishlist_crud,name='wishlist_crud'),
+  path('',views.wishlist_view,name='wishlist_view'),
+  path('<int:id>', views.wishlist_add, name='wishlist_add'),
   path('<int:id>', views.wishlist_delete, name='wishlist_delete'),
 ]
--- wishlist/views.py
+++ wishlist/views.py
@@ -9,80 +9,53 @@ from django.core.paginator import Paginator
 from authentication.views import is_auth
       
 
-@api_view(['GET','POST'])
-# @permission_classes([is_auth])
-def wishlist_crud(request):
-   if request.method=='GET':
-      #   token=request.COOKIES.get('token')
-      if request.user.is_authenticated:
-        custom_user=CustomUser.objects.get(email=request.user.email)
-        wishlist=Wishlist.objects.filter(user_id=custom_user)
-        serializer=WishlistSerializer(wishlist, many=True)
-        paginated=Paginator(wishlist , 5)
-        page_number=request.GET.get('page')
-        page_obj=paginated.get_page(page_number)
-        return Response({"data":serializer.data,"pagination":page_obj})
-      else:
-         return Response({"message":"User is not logged in"})
+@api_view()
+@permission_classes([is_auth])
+def wishlist_view(request):
+   if request.method == 'GET':
+            token = request.COOKIES.get('token')      
+            decoded_token = jwt.decode(token, "PROJECT!@#%^2434", algorithms=["HS256"])
+            user_id = decoded_token.get('user_id')
+            wishlist = Wishlist.objects.filter(user_id=user_id).order_by('id').select_related('product_id')         
+            # serializer = WishlistSerializer(wishlist, many=True)
+            # paginated = Paginator(wishlist, 5)
+            # page_number = request.GET.get('page')
+            # page_obj = paginated.get_page(page_number)
+            paginated = Paginator(wishlist, 5)
+            page_number = request.GET.get('page')
+            page_obj = paginated.get_page(page_number)
+            serializer = WishlistSerializer(page_obj, many=True)
+            return Response({"data": serializer.data})            
 
-# def wishlist_crud(request):
-#    if request.method == 'GET':
-#         token = request.COOKIES.get('token')
-#         if token:
-#             decoded_token = jwt.decode(token, "PROJECT!@#%^2434", algorithms=["HS256"])
-#             user_id = decoded_token['user_id']
-#             custom_user = CustomUser.objects.get(id=user_id)
-#             wishlist = Wishlist.objects.filter(user_id=custom_user.id).order_by('id')            
-#             serializer = WishlistSerializer(wishlist, many=True)
-#             paginated = Paginator(wishlist, 5)
-#             page_number = request.GET.get('page')
-#             page_obj = paginated.get_page(page_number)
-#             return Response({"data": serializer.data, "pagination":page_obj})
-#         else:
-#          decoded_token = jwt.decode(token, "PROJECT!@#%^2434", "HS256")
-#          return Response(token)
-
-    
-   if request.method=='POST':
-         if request.user.is_authenticated:
-           serializer = WishlistSerializer(data=request.data)
-         #  serializer = WishlistSerializer(data=request.data, context={'request': request})
-           if serializer.is_valid():
+@api_view(['POST'])
+@permission_classes([is_auth])
+def wishlist_add(request,id):
+ if request.method=='POST':
+         data = {'product_id': id}
+         serializer = WishlistSerializer(data=data, context={'request': request})  
+         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
-           else:
-            return Response(serializer.errors)
          else:
-           return Response({"message":"User is not logged in"})
-   
-# @api_view(['DELETE'])
-# def wishlist_delete(request, id):
-#    if request.method=='DELETE':
-#          if request.user.is_authenticated:
-#             # product_id=int(request.GET('product_id'))
-#             product_id = int(request.GET['product_id'])
-#             if(Wishlist.objects.filter(user=request.user,product_id=product_id)):
-#                wishlistItem=Wishlist.objects.get(product_id=product_id)
-#                wishlistItem.delete()
-#                return Response({"message": "Product deleted"})
+             return Response(serializer.errors)
+
+
 
 @api_view(['DELETE'])
-# @permission_classes([is_auth])
-def wishlist_delete(request):
-   if request.method == 'DELETE':
-         token=request.COOKIES.get('token')
-      # if token.is_authenticated:
-         product_id = int(request.GET['product_id'])
-         if Wishlist.objects.filter(user=token.user, product_id=product_id).exists():
-            wishlist_item = Wishlist.objects.get(user=request.user, product_id=product_id)
+@permission_classes([is_auth])
+def wishlist_delete(request, id):
+    if request.method == 'DELETE':
+        token = request.COOKIES.get('token')
+        decoded_token = jwt.decode(token, "PROJECT!@#%^2434", algorithms=["HS256"])
+        user_id = decoded_token.get('user_id')
+      #   try:
+        if Wishlist.objects.filter(user_id=user_id,id=id).exists():
+            wishlist_item = Wishlist.objects.get(id=id, user_id=user_id)
             wishlist_item.delete()
             return Response({"message": "Product deleted"})
-         else:
+      #   except Wishlist.DoesNotExist:
+        else:
             return Response({"message": "Product not found"})
-      # else:
-      #    return Response({"message": "User not authenticated"})
-
-
 
          
 
