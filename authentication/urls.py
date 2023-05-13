76f7e921f3e49fbcc039b83cda1e7405145fa4ce
Muhammed Adel <muhammed.adel.elshall@gmail.com>
2023-05-13 21:19:17 +0300

    add my order, my cart, my wishlist

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from knox import views as knox_views
-from .views import RegisterAPI, LoginAPI, LogoutAPI, ProfileAPIView
+from .views import RegisterAPI, LoginAPI, LogoutAPI, ProfileAPIView, MyOrder
 
 urlpatterns = [
     path('register/', RegisterAPI.as_view(), name='register'),
     path('login/', LoginAPI.as_view(), name='login'),
     path('logout/', LogoutAPI.as_view(), name='logout'),
     path('profile/', ProfileAPIView.as_view(), name='profile'),
+    path('myorders/', MyOrder.as_view(), name='myorders'),
 ]
