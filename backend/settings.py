c2829727282bef7d4c55da08505c3c6e13e80e86
Muhammed Adel <muhammed.adel.elshall@gmail.com>
2023-05-22 21:24:06 +0300

    static

--- backend/settings.py
+++ backend/settings.py
@@ -180,10 +180,10 @@ PAYPAL_RECEIVER_EMAIL=os.environ.get('PAYPAL_RECEIVER_EMAIL'),
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'staticfiles_build/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles_build', 'static'),
 ]
 
 # Default primary key field type
