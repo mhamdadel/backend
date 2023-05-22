a7ad144e776d03912aae1746919220b754e24f98
Muhammed Adel <muhammed.adel.elshall@gmail.com>
2023-05-22 22:00:04 +0300

    a

--- backend/settings.py
+++ backend/settings.py
@@ -182,9 +182,10 @@ PAYPAL_RECEIVER_EMAIL=os.environ.get('PAYPAL_RECEIVER_EMAIL'),
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'staticfiles/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'staticfiles'),
+    "/var/www/staticfiles/",
 ]
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
