ad452c4f0d786a2de893bd3cf9daa9ac13ae9056
Muhammed Adel <muhammed.adel.elshall@gmail.com>
2023-05-22 21:14:31 +0300

    add statics

--- backend/settings.py
+++ backend/settings.py
@@ -181,7 +181,7 @@ PAYPAL_RECEIVER_EMAIL=os.environ.get('PAYPAL_RECEIVER_EMAIL'),
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
